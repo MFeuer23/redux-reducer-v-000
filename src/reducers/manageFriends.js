1e2e0278fc2d07ba5db5c960f6ebdcb2ec025e68
Mallory Feuer <mallory.feuer@gmail.com>
2019-01-30 18:40:36 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,8 @@
 export function manageFriends(state, action){
+   switch (action.type) {
+    case 'ADD_FRIEND':
+      return {count: state.numberOfPresents + 1}
+    default:
+      return state;
+  }
 }
